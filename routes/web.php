022c2a885aa087f8b38f8f270331035827dc5af3
N-Necoze <n.nishidaneco@gmail.com>
2022-09-19 16:23:00 +0900

    3rd commit

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('index');
+});
+
 
 Route::get('/posts', 'PostController@index');
 
-#Route::get('/', function () {
-#    return view('welcome');
-#});
+
